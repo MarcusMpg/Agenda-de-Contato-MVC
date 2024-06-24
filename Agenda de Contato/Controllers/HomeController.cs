e8233f1595bd470b64db6c13a05accaf8087c9cc
Marcus Mathues <107503566+marcusmpg@users.noreply.github.com>
2024-06-24 12:11:28 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda de Contato/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Agenda_de_Contato.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Agenda_de_Contato.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Agenda de Contato/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Agenda_de_Contato.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
