5be309acd6aa1daa4e1fcdb34fe2fc6b4614b21b
Marcus Mathues <107503566+marcusmpg@users.noreply.github.com>
2024-07-01 09:04:14 -0300

    implementado as validaçoes de cadastro e finaliando o sistema de agenda

--- Agenda de Contato/Controllers/Contato.cs
+++ Agenda de Contato/Controllers/Contato.cs
@@ -1,10 +1,11 @@
 ﻿using Agenda_de_Contato.Models;
 using Agenda_de_Contato.Repositorio;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Agenda_de_Contato.Controllers
 {
-    
+
     public class Contato : Controller
     {
         private readonly IContatoRepositorio _IContatoRepositorio;
@@ -14,7 +15,7 @@ namespace Agenda_de_Contato.Controllers
         }
         public IActionResult Index()
         {
-           List<ContatoModel> contatos = _IContatoRepositorio.BuscarTodos();
+            List<ContatoModel> contatos = _IContatoRepositorio.BuscarTodos();
             return View(contatos);
         }
         public IActionResult Criar()
@@ -23,7 +24,7 @@ namespace Agenda_de_Contato.Controllers
         }
         public IActionResult Editar(int id)
         {
-           ContatoModel contato = _IContatoRepositorio.ListarPorId(id);   
+            ContatoModel contato = _IContatoRepositorio.ListarPorId(id);
             return View(contato);
         }
         public IActionResult ExcluirSpan(int id)
@@ -34,22 +35,70 @@ namespace Agenda_de_Contato.Controllers
 
         public IActionResult Excluir(int id)
         {
-            _IContatoRepositorio.Excluir(id);
-            return RedirectToAction("Index");
+            try
+            {
+                bool excluido = _IContatoRepositorio.Excluir(id);
+
+                if (excluido)
+                {
+                    TempData["MensagemSucesso"] = "Apagado com sucesso!";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = "Ops, Não foi possivel Apagar seu contato!, Tente novamente!";
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, Não foi possivel cadastrar seu contato!, Tente novamente, Detalhe do erro :{erro.Message}";
+                return RedirectToAction("Index");
+            }
+
         }
 
 
         [HttpPost]
-        public IActionResult Criar(ContatoModel contato) 
+        public IActionResult Criar(ContatoModel contato)
         {
-        _IContatoRepositorio.Adicionar(contato);
-            return RedirectToAction("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _IContatoRepositorio.Adicionar(contato);
+                    TempData["MensagemSucesso"] = "Cadastrado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+                return View(contato);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, Não foi possivel cadastrar seu contato!, Tente novamente, Detalhe do erro :{erro.Message}";
+                return RedirectToAction("Index");
+            }
+
         }
         [HttpPost]
         public IActionResult Editar(ContatoModel contato)
         {
-            _IContatoRepositorio.Editar(contato);
-            return RedirectToAction("Index");
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _IContatoRepositorio.Editar(contato);
+                    TempData["MensagemSucesso"] = "Editado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+                return View(contato);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, Não foi possivel editado seu contato!, Tente novamente, Detalhe do erro :{erro.Message}";
+                return RedirectToAction("Index");
+
+            }
+
         }
     }
 }
--- Agenda de Contato/Models/ContatoModel.cs
+++ Agenda de Contato/Models/ContatoModel.cs
@@ -1,10 +1,17 @@
-﻿namespace Agenda_de_Contato.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda_de_Contato.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Digite o nome do contato!")]
         public string Nome { get; set; }
+        [Required(ErrorMessage ="Digite o e-mail do contato")]
+        [EmailAddress(ErrorMessage ="Digite um e-mail valido!")]
         public string Email { get; set; }
+        [Required(ErrorMessage ="Digite o número do contato")]
+        [Phone(ErrorMessage ="Digite um número valido!")]
         public string Numero { get; set; }
     }
 }
