533738a49e0a3570f64b2fa6227e5fbda649835a
Marcus Mathues <107503566+marcusmpg@users.noreply.github.com>
2024-07-01 09:44:01 -0300

    implementando sistema de busca com jquery datatable

--- Agenda de Contato/wwwroot/js/site.js
+++ Agenda de Contato/wwwroot/js/site.js
@@ -2,6 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let table = new DataTable('#table-contatos');
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 })
